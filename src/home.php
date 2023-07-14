027241e5809570af27279402e06a62b467ee0113
Felipe Paez <ofpaez87@gmail.com>
2023-07-13 19:28:03 -0500

    css básico

--- src/home.php
+++ src/home.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="src/resources/main.css">
 </head>
 <body>
-    <h1>Bienvenido a mi Blog</h1>
-    <?php
-    use Phelo\Blog\models\Post;
-    
-    $posts = Post::getPosts();
+    <?php require 'src/resources/navbar.php'; ?>
+    <div class="post-container">
+        <h1>Bienvenido a mi Blog</h1>
+        <?php
+        use Phelo\Blog\models\Post;
+        
+        $posts = Post::getPosts();
 
-    foreach($posts as $post){
-        echo "<div><a href='{$post->getUrl()}'>{$post->getPostName()}</a></div>";
-    }
-    ?>
+        foreach($posts as $post){
+            echo "<div><a href='{$post->getUrl()}' class='post'>{$post->getPostName()}</a></div>";
+        }
+        ?>
+
+    </div>
 </body>
 </html>
---
+++ src/resources/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li>
+            <a href="?view=home">HOME</a>
+        </li>
+    </ul>
+</nav>
